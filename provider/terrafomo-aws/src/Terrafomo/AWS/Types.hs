2d1277c0ab2722fa968d6bd60ee5eeef5f0146b2
Nick Hibberd <nhibberd@gmail.com>
2018-08-08 16:00:51 -0700

    Add override for aws_s3_bucket logging

--- provider/terrafomo-aws/src/Terrafomo/AWS/Types.hs
+++ provider/terrafomo-aws/src/Terrafomo/AWS/Types.hs
@@ -209,6 +209,23 @@ data S3BucketVersioning s = S3BucketVersioning
 
 $(TH.makeObject ''S3BucketVersioning)
 
+data S3BucketLogging s = S3BucketLogging
+    { _target_bucket    :: !(Attr s Text)
+    -- ^ Specifies the bucket where you want Amazon S3 to store server
+    -- access logs. You can have your logs delivered to any bucket
+    -- that you own, including the same bucket that is being
+    -- logged. You can also configure multiple buckets to deliver
+    -- their logs to the same target bucket. In this case you should
+    -- choose a different TargetPrefix for each source bucket so that
+    -- the delivered log files can be distinguished by key.
+    , _target_prefix :: !(Attr s Text)
+    -- ^ This element lets you specify a prefix for the keys that the
+    -- log files will be stored under.
+    } deriving (Show, Eq, Generic)
+
+$(TH.makeObject ''S3BucketLogging)
+
+
 -- DynamoDB
 
 data DynamoTableAttribute = DynamoTableAttribute !Text !DynamoAttributeType
