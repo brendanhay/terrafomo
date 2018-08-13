f77da09761ee0d07aca4a15989ba14f3290174d7
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-13 10:09:05 +0200

    Regenerating terrafomo-logicmonitor

--- provider/terrafomo-logicmonitor/gen/Terrafomo/LogicMonitor/Provider.hs
+++ provider/terrafomo-logicmonitor/gen/Terrafomo/LogicMonitor/Provider.hs
@@ -30,17 +30,18 @@ import GHC.Base (($))
 
 import Terrafomo.LogicMonitor.Settings
 
-import qualified Data.Hashable               as P
-import qualified Data.HashMap.Strict         as P
-import qualified Data.List.NonEmpty          as P
-import qualified Data.Text                   as P
-import qualified GHC.Generics                as P
-import qualified Lens.Micro                  as P
-import qualified Prelude                     as P
-import qualified Terrafomo.HCL               as TF
-import qualified Terrafomo.LogicMonitor.Lens as P
-import qualified Terrafomo.Name              as TF
-import qualified Terrafomo.Provider          as TF
+import qualified Data.Hashable                as P
+import qualified Data.HashMap.Strict          as P
+import qualified Data.List.NonEmpty           as P
+import qualified Data.Text                    as P
+import qualified GHC.Generics                 as P
+import qualified Lens.Micro                   as P
+import qualified Prelude                      as P
+import qualified Terrafomo.HCL                as TF
+import qualified Terrafomo.LogicMonitor.Lens  as P
+import qualified Terrafomo.LogicMonitor.Types as P
+import qualified Terrafomo.Name               as TF
+import qualified Terrafomo.Provider           as TF
 
 -- | The @LogicMonitor@ Terraform provider configuration.
 --
@@ -74,7 +75,7 @@ instance TF.IsSection Provider where
                   ])
 
 instance TF.IsProvider Provider where
-    type ProviderType Provider = "logicmonitor"
+    type ProviderType Provider = "provider"
 
 newProvider
     :: P.Text -- ^ @api_id@ - 'P.apiId'
--- provider/terrafomo-logicmonitor/gen/Terrafomo/LogicMonitor/Settings.hs
+++ provider/terrafomo-logicmonitor/gen/Terrafomo/LogicMonitor/Settings.hs
@@ -26,6 +26,8 @@ module Terrafomo.LogicMonitor.Settings
 import Data.Functor ((<$>))
 import Data.Maybe   (catMaybes)
 
+import GHC.Base (($))
+
 import qualified Data.Hashable                as P
 import qualified Data.HashMap.Strict          as P
 import qualified Data.List.NonEmpty           as P
