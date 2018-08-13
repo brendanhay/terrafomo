0d1711b6aa275ec8b2de13d5434abd5aada471fd
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-13 10:09:07 +0200

    Regenerating terrafomo-powerdns

--- provider/terrafomo-powerdns/gen/Terrafomo/PowerDNS/Settings.hs
+++ provider/terrafomo-powerdns/gen/Terrafomo/PowerDNS/Settings.hs
@@ -22,6 +22,8 @@ module Terrafomo.PowerDNS.Settings
 import Data.Functor ((<$>))
 import Data.Maybe   (catMaybes)
 
+import GHC.Base (($))
+
 import qualified Data.Hashable            as P
 import qualified Data.HashMap.Strict      as P
 import qualified Data.List.NonEmpty       as P
