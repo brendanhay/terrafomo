b2145375a03ea9c9f0676ae49e31fd98026723c7
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-13 18:35:26 +0200

    Regenerating terrafomo-bitbucket

--- provider/terrafomo-bitbucket/gen/Terrafomo/Bitbucket/Provider.hs
+++ provider/terrafomo-bitbucket/gen/Terrafomo/Bitbucket/Provider.hs
@@ -1,6 +1,7 @@
 -- This module is auto-generated.
 
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE StrictData        #-}
 
@@ -23,7 +24,6 @@ module Terrafomo.Bitbucket.Provider
 
 import Data.Function ((&))
 import Data.Functor  ((<$>))
-import Data.Maybe    (catMaybes)
 import Data.Proxy    (Proxy (Proxy))
 
 import GHC.Base (($))
@@ -32,7 +32,10 @@ import Terrafomo.Bitbucket.Settings
 
 import qualified Data.Hashable             as P
 import qualified Data.HashMap.Strict       as P
+import qualified Data.HashMap.Strict       as Map
 import qualified Data.List.NonEmpty        as P
+import qualified Data.Maybe                as P
+import qualified Data.Monoid               as P
 import qualified Data.Text                 as P
 import qualified GHC.Generics              as P
 import qualified Lens.Micro                as P
@@ -42,6 +45,7 @@ import qualified Terrafomo.Bitbucket.Types as P
 import qualified Terrafomo.HCL             as TF
 import qualified Terrafomo.Name            as TF
 import qualified Terrafomo.Provider        as TF
+import qualified Terrafomo.Validator       as TF
 
 -- | The @Bitbucket@ Terraform provider configuration.
 --
@@ -56,23 +60,6 @@ data Provider = Provider'
     --
     } deriving (P.Show, P.Eq, P.Generic)
 
-instance P.Hashable Provider
-
-instance TF.IsSection Provider where
-    toSection x@Provider'{..} =
-        let typ = TF.providerType (Proxy :: Proxy (Provider))
-            key = TF.providerKey x
-         in TF.section "provider" [TF.type_ typ]
-          & TF.pairs
-              (catMaybes
-                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
-                  , P.Just $ TF.assign "password" _password
-                  , P.Just $ TF.assign "username" _username
-                  ])
-
-instance TF.IsProvider Provider where
-    type ProviderType Provider = "provider"
-
 newProvider
     :: P.Text -- ^ @password@ - 'P.password'
     -> P.Text -- ^ @username@ - 'P.username'
@@ -83,14 +70,32 @@ newProvider _password _username =
         , _username = _username
         }
 
+instance P.Hashable Provider
+
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy (Provider))
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "password" _password
+                  , P.Just $ TF.assign "username" _username
+                  ])
+
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "provider"
+
+instance TF.IsValid (Provider) where
+    validator = P.mempty
+
 instance P.HasPassword (Provider) (P.Text) where
     password =
         P.lens (_password :: Provider -> P.Text)
-               (\s a -> s { _password = a
-                          } :: Provider)
+               (\s a -> s { _password = a } :: Provider)
 
 instance P.HasUsername (Provider) (P.Text) where
     username =
         P.lens (_username :: Provider -> P.Text)
-               (\s a -> s { _username = a
-                          } :: Provider)
+               (\s a -> s { _username = a } :: Provider)
--- provider/terrafomo-bitbucket/gen/Terrafomo/Bitbucket/Resource.hs
+++ provider/terrafomo-bitbucket/gen/Terrafomo/Bitbucket/Resource.hs
@@ -1,6 +1,7 @@
 -- This module is auto-generated.
 
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE StrictData        #-}
 
@@ -32,7 +33,6 @@ module Terrafomo.Bitbucket.Resource
     ) where
 
 import Data.Functor ((<$>))
-import Data.Maybe   (catMaybes)
 
 import GHC.Base (($))
 
@@ -40,7 +40,10 @@ import Terrafomo.Bitbucket.Settings
 
 import qualified Data.Hashable                as P
 import qualified Data.HashMap.Strict          as P
+import qualified Data.HashMap.Strict          as Map
 import qualified Data.List.NonEmpty           as P
+import qualified Data.Maybe                   as P
+import qualified Data.Monoid                  as P
 import qualified Data.Text                    as P
 import qualified GHC.Generics                 as P
 import qualified Lens.Micro                   as P
@@ -52,6 +55,7 @@ import qualified Terrafomo.Bitbucket.Types    as P
 import qualified Terrafomo.HCL                as TF
 import qualified Terrafomo.Name               as TF
 import qualified Terrafomo.Schema             as TF
+import qualified Terrafomo.Validator          as TF
 
 -- | @bitbucket_default_reviewers@ Resource.
 --
@@ -64,48 +68,48 @@ data DefaultReviewersResource s = DefaultReviewersResource'
     , _repository :: TF.Attr s P.Text
     -- ^ @repository@ - (Required)
     --
-    , _reviewers  :: TF.Attr s [TF.Attr s (TF.Attr s P.Text)]
+    , _reviewers  :: TF.Attr s [TF.Attr s P.Text]
     -- ^ @reviewers@ - (Required)
     --
     } deriving (P.Show, P.Eq, P.Generic)
 
-instance TF.IsObject (DefaultReviewersResource s) where
-    toObject DefaultReviewersResource'{..} = catMaybes
-        [ TF.assign "owner" <$> TF.attribute _owner
-        , TF.assign "repository" <$> TF.attribute _repository
-        , TF.assign "reviewers" <$> TF.attribute _reviewers
-        ]
-
 defaultReviewersResource
     :: TF.Attr s P.Text -- ^ @owner@ - 'P.owner'
     -> TF.Attr s P.Text -- ^ @repository@ - 'P.repository'
-    -> TF.Attr s [TF.Attr s (TF.Attr s P.Text)] -- ^ @reviewers@ - 'P.reviewers'
+    -> TF.Attr s [TF.Attr s P.Text] -- ^ @reviewers@ - 'P.reviewers'
     -> TF.Resource P.Provider (DefaultReviewersResource s)
 defaultReviewersResource _owner _repository _reviewers =
-    TF.newResource "bitbucket_default_reviewers" $
+    TF.newResource "bitbucket_default_reviewers" TF.validator $
         DefaultReviewersResource'
             { _owner = _owner
             , _repository = _repository
             , _reviewers = _reviewers
             }
 
+instance TF.IsObject (DefaultReviewersResource s) where
+    toObject DefaultReviewersResource'{..} = P.catMaybes
+        [ TF.assign "owner" <$> TF.attribute _owner
+        , TF.assign "repository" <$> TF.attribute _repository
+        , TF.assign "reviewers" <$> TF.attribute _reviewers
+        ]
+
+instance TF.IsValid (DefaultReviewersResource s) where
+    validator = P.mempty
+
 instance P.HasOwner (DefaultReviewersResource s) (TF.Attr s P.Text) where
     owner =
         P.lens (_owner :: DefaultReviewersResource s -> TF.Attr s P.Text)
-               (\s a -> s { _owner = a
-                          } :: DefaultReviewersResource s)
+               (\s a -> s { _owner = a } :: DefaultReviewersResource s)
 
 instance P.HasRepository (DefaultReviewersResource s) (TF.Attr s P.Text) where
     repository =
         P.lens (_repository :: DefaultReviewersResource s -> TF.Attr s P.Text)
-               (\s a -> s { _repository = a
-                          } :: DefaultReviewersResource s)
+               (\s a -> s { _repository = a } :: DefaultReviewersResource s)
 
-instance P.HasReviewers (DefaultReviewersResource s) (TF.Attr s [TF.Attr s (TF.Attr s P.Text)]) where
+instance P.HasReviewers (DefaultReviewersResource s) (TF.Attr s [TF.Attr s P.Text]) where
     reviewers =
-        P.lens (_reviewers :: DefaultReviewersResource s -> TF.Attr s [TF.Attr s (TF.Attr s P.Text)])
-               (\s a -> s { _reviewers = a
-                          } :: DefaultReviewersResource s)
+        P.lens (_reviewers :: DefaultReviewersResource s -> TF.Attr s [TF.Attr s P.Text])
+               (\s a -> s { _reviewers = a } :: DefaultReviewersResource s)
 
 -- | @bitbucket_hook@ Resource.
 --
@@ -118,7 +122,7 @@ data HookResource s = HookResource'
     , _description          :: TF.Attr s P.Text
     -- ^ @description@ - (Required)
     --
-    , _events               :: TF.Attr s [TF.Attr s (TF.Attr s P.Text)]
+    , _events               :: TF.Attr s [TF.Attr s P.Text]
     -- ^ @events@ - (Required)
     --
     , _owner                :: TF.Attr s P.Text
@@ -135,26 +139,15 @@ data HookResource s = HookResource'
     --
     } deriving (P.Show, P.Eq, P.Generic)
 
-instance TF.IsObject (HookResource s) where
-    toObject HookResource'{..} = catMaybes
-        [ TF.assign "active" <$> TF.attribute _active
-        , TF.assign "description" <$> TF.attribute _description
-        , TF.assign "events" <$> TF.attribute _events
-        , TF.assign "owner" <$> TF.attribute _owner
-        , TF.assign "repository" <$> TF.attribute _repository
-        , TF.assign "skip_cert_verification" <$> TF.attribute _skipCertVerification
-        , TF.assign "url" <$> TF.attribute _url
-        ]
-
 hookResource
     :: TF.Attr s P.Text -- ^ @description@ - 'P.description'
-    -> TF.Attr s [TF.Attr s (TF.Attr s P.Text)] -- ^ @events@ - 'P.events'
+    -> TF.Attr s [TF.Attr s P.Text] -- ^ @events@ - 'P.events'
     -> TF.Attr s P.Text -- ^ @owner@ - 'P.owner'
     -> TF.Attr s P.Text -- ^ @repository@ - 'P.repository'
     -> TF.Attr s P.Text -- ^ @url@ - 'P.url'
     -> TF.Resource P.Provider (HookResource s)
 hookResource _description _events _owner _repository _url =
-    TF.newResource "bitbucket_hook" $
+    TF.newResource "bitbucket_hook" TF.validator $
         HookResource'
             { _active = TF.value P.True
             , _description = _description
@@ -165,47 +158,54 @@ hookResource _description _events _owner _repository _url =
             , _url = _url
             }
 
+instance TF.IsObject (HookResource s) where
+    toObject HookResource'{..} = P.catMaybes
+        [ TF.assign "active" <$> TF.attribute _active
+        , TF.assign "description" <$> TF.attribute _description
+        , TF.assign "events" <$> TF.attribute _events
+        , TF.assign "owner" <$> TF.attribute _owner
+        , TF.assign "repository" <$> TF.attribute _repository
+        , TF.assign "skip_cert_verification" <$> TF.attribute _skipCertVerification
+        , TF.assign "url" <$> TF.attribute _url
+        ]
+
+instance TF.IsValid (HookResource s) where
+    validator = P.mempty
+
 instance P.HasActive (HookResource s) (TF.Attr s P.Bool) where
     active =
         P.lens (_active :: HookResource s -> TF.Attr s P.Bool)
-               (\s a -> s { _active = a
-                          } :: HookResource s)
+               (\s a -> s { _active = a } :: HookResource s)
 
 instance P.HasDescription (HookResource s) (TF.Attr s P.Text) where
     description =
         P.lens (_description :: HookResource s -> TF.Attr s P.Text)
-               (\s a -> s { _description = a
-                          } :: HookResource s)
+               (\s a -> s { _description = a } :: HookResource s)
 
-instance P.HasEvents (HookResource s) (TF.Attr s [TF.Attr s (TF.Attr s P.Text)]) where
+instance P.HasEvents (HookResource s) (TF.Attr s [TF.Attr s P.Text]) where
     events =
-        P.lens (_events :: HookResource s -> TF.Attr s [TF.Attr s (TF.Attr s P.Text)])
-               (\s a -> s { _events = a
-                          } :: HookResource s)
+        P.lens (_events :: HookResource s -> TF.Attr s [TF.Attr s P.Text])
+               (\s a -> s { _events = a } :: HookResource s)
 
 instance P.HasOwner (HookResource s) (TF.Attr s P.Text) where
     owner =
         P.lens (_owner :: HookResource s -> TF.Attr s P.Text)
-               (\s a -> s { _owner = a
-                          } :: HookResource s)
+               (\s a -> s { _owner = a } :: HookResource s)
 
 instance P.HasRepository (HookResource s) (TF.Attr s P.Text) where
     repository =
         P.lens (_repository :: HookResource s -> TF.Attr s P.Text)
-               (\s a -> s { _repository = a
-                          } :: HookResource s)
+               (\s a -> s { _repository = a } :: HookResource s)
 
 instance P.HasSkipCertVerification (HookResource s) (TF.Attr s P.Bool) where
     skipCertVerification =
         P.lens (_skipCertVerification :: HookResource s -> TF.Attr s P.Bool)
-               (\s a -> s { _skipCertVerification = a
-                          } :: HookResource s)
+               (\s a -> s { _skipCertVerification = a } :: HookResource s)
 
 instance P.HasUrl (HookResource s) (TF.Attr s P.Text) where
     url =
         P.lens (_url :: HookResource s -> TF.Attr s P.Text)
-               (\s a -> s { _url = a
-                          } :: HookResource s)
+               (\s a -> s { _url = a } :: HookResource s)
 
 instance s ~ s' => P.HasComputedUuid (TF.Ref s' (HookResource s)) (TF.Attr s P.Text) where
     computedUuid x = TF.compute (TF.refKey x) "_computedUuid"
@@ -250,27 +250,12 @@ data RepositoryResource s = RepositoryResource'
     --
     } deriving (P.Show, P.Eq, P.Generic)
 
-instance TF.IsObject (RepositoryResource s) where
-    toObject RepositoryResource'{..} = catMaybes
-        [ TF.assign "description" <$> TF.attribute _description
-        , TF.assign "fork_policy" <$> TF.attribute _forkPolicy
-        , TF.assign "has_issues" <$> TF.attribute _hasIssues
-        , TF.assign "has_wiki" <$> TF.attribute _hasWiki
-        , TF.assign "is_private" <$> TF.attribute _isPrivate
-        , TF.assign "language" <$> TF.attribute _language
-        , TF.assign "name" <$> TF.attribute _name
-        , TF.assign "owner" <$> TF.attribute _owner
-        , TF.assign "project_key" <$> TF.attribute _projectKey
-        , TF.assign "scm" <$> TF.attribute _scm
-        , TF.assign "website" <$> TF.attribute _website
-        ]
-
 repositoryResource
     :: TF.Attr s P.Text -- ^ @name@ - 'P.name'
     -> TF.Attr s P.Text -- ^ @owner@ - 'P.owner'
     -> TF.Resource P.Provider (RepositoryResource s)
 repositoryResource _name _owner =
-    TF.newResource "bitbucket_repository" $
+    TF.newResource "bitbucket_repository" TF.validator $
         RepositoryResource'
             { _description = TF.Nil
             , _forkPolicy = TF.value "allow_forks"
@@ -285,71 +270,78 @@ repositoryResource _name _owner =
             , _website = TF.Nil
             }
 
+instance TF.IsObject (RepositoryResource s) where
+    toObject RepositoryResource'{..} = P.catMaybes
+        [ TF.assign "description" <$> TF.attribute _description
+        , TF.assign "fork_policy" <$> TF.attribute _forkPolicy
+        , TF.assign "has_issues" <$> TF.attribute _hasIssues
+        , TF.assign "has_wiki" <$> TF.attribute _hasWiki
+        , TF.assign "is_private" <$> TF.attribute _isPrivate
+        , TF.assign "language" <$> TF.attribute _language
+        , TF.assign "name" <$> TF.attribute _name
+        , TF.assign "owner" <$> TF.attribute _owner
+        , TF.assign "project_key" <$> TF.attribute _projectKey
+        , TF.assign "scm" <$> TF.attribute _scm
+        , TF.assign "website" <$> TF.attribute _website
+        ]
+
+instance TF.IsValid (RepositoryResource s) where
+    validator = P.mempty
+
 instance P.HasDescription (RepositoryResource s) (TF.Attr s P.Text) where
     description =
         P.lens (_description :: RepositoryResource s -> TF.Attr s P.Text)
-               (\s a -> s { _description = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _description = a } :: RepositoryResource s)
 
 instance P.HasForkPolicy (RepositoryResource s) (TF.Attr s P.Text) where
     forkPolicy =
         P.lens (_forkPolicy :: RepositoryResource s -> TF.Attr s P.Text)
-               (\s a -> s { _forkPolicy = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _forkPolicy = a } :: RepositoryResource s)
 
 instance P.HasHasIssues (RepositoryResource s) (TF.Attr s P.Bool) where
     hasIssues =
         P.lens (_hasIssues :: RepositoryResource s -> TF.Attr s P.Bool)
-               (\s a -> s { _hasIssues = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _hasIssues = a } :: RepositoryResource s)
 
 instance P.HasHasWiki (RepositoryResource s) (TF.Attr s P.Bool) where
     hasWiki =
         P.lens (_hasWiki :: RepositoryResource s -> TF.Attr s P.Bool)
-               (\s a -> s { _hasWiki = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _hasWiki = a } :: RepositoryResource s)
 
 instance P.HasIsPrivate (RepositoryResource s) (TF.Attr s P.Bool) where
     isPrivate =
         P.lens (_isPrivate :: RepositoryResource s -> TF.Attr s P.Bool)
-               (\s a -> s { _isPrivate = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _isPrivate = a } :: RepositoryResource s)
 
 instance P.HasLanguage (RepositoryResource s) (TF.Attr s P.Text) where
     language =
         P.lens (_language :: RepositoryResource s -> TF.Attr s P.Text)
-               (\s a -> s { _language = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _language = a } :: RepositoryResource s)
 
 instance P.HasName (RepositoryResource s) (TF.Attr s P.Text) where
     name =
         P.lens (_name :: RepositoryResource s -> TF.Attr s P.Text)
-               (\s a -> s { _name = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _name = a } :: RepositoryResource s)
 
 instance P.HasOwner (RepositoryResource s) (TF.Attr s P.Text) where
     owner =
         P.lens (_owner :: RepositoryResource s -> TF.Attr s P.Text)
-               (\s a -> s { _owner = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _owner = a } :: RepositoryResource s)
 
 instance P.HasProjectKey (RepositoryResource s) (TF.Attr s P.Text) where
     projectKey =
         P.lens (_projectKey :: RepositoryResource s -> TF.Attr s P.Text)
-               (\s a -> s { _projectKey = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _projectKey = a } :: RepositoryResource s)
 
 instance P.HasScm (RepositoryResource s) (TF.Attr s P.Text) where
     scm =
         P.lens (_scm :: RepositoryResource s -> TF.Attr s P.Text)
-               (\s a -> s { _scm = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _scm = a } :: RepositoryResource s)
 
 instance P.HasWebsite (RepositoryResource s) (TF.Attr s P.Text) where
     website =
         P.lens (_website :: RepositoryResource s -> TF.Attr s P.Text)
-               (\s a -> s { _website = a
-                          } :: RepositoryResource s)
+               (\s a -> s { _website = a } :: RepositoryResource s)
 
 instance s ~ s' => P.HasComputedCloneHttps (TF.Ref s' (RepositoryResource s)) (TF.Attr s P.Text) where
     computedCloneHttps x = TF.compute (TF.refKey x) "_computedCloneHttps"
