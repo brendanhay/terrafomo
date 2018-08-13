417a97497fbe5f7e5571355f4b932c6857e4be6e
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-13 18:35:33 +0200

    Regenerating terrafomo-powerdns

--- provider/terrafomo-powerdns/gen/Terrafomo/PowerDNS/Provider.hs
+++ provider/terrafomo-powerdns/gen/Terrafomo/PowerDNS/Provider.hs
@@ -1,6 +1,7 @@
 -- This module is auto-generated.
 
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE StrictData        #-}
 
@@ -23,7 +24,6 @@ module Terrafomo.PowerDNS.Provider
 
 import Data.Function ((&))
 import Data.Functor  ((<$>))
-import Data.Maybe    (catMaybes)
 import Data.Proxy    (Proxy (Proxy))
 
 import GHC.Base (($))
@@ -32,7 +32,10 @@ import Terrafomo.PowerDNS.Settings
 
 import qualified Data.Hashable            as P
 import qualified Data.HashMap.Strict      as P
+import qualified Data.HashMap.Strict      as Map
 import qualified Data.List.NonEmpty       as P
+import qualified Data.Maybe               as P
+import qualified Data.Monoid              as P
 import qualified Data.Text                as P
 import qualified GHC.Generics             as P
 import qualified Lens.Micro               as P
@@ -42,6 +45,7 @@ import qualified Terrafomo.Name           as TF
 import qualified Terrafomo.PowerDNS.Lens  as P
 import qualified Terrafomo.PowerDNS.Types as P
 import qualified Terrafomo.Provider       as TF
+import qualified Terrafomo.Validator      as TF
 
 -- | The @PowerDNS@ Terraform provider configuration.
 --
@@ -58,23 +62,6 @@ data Provider = Provider'
     --
     } deriving (P.Show, P.Eq, P.Generic)
 
-instance P.Hashable Provider
-
-instance TF.IsSection Provider where
-    toSection x@Provider'{..} =
-        let typ = TF.providerType (Proxy :: Proxy (Provider))
-            key = TF.providerKey x
-         in TF.section "provider" [TF.type_ typ]
-          & TF.pairs
-              (catMaybes
-                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
-                  , P.Just $ TF.assign "api_key" _apiKey
-                  , P.Just $ TF.assign "server_url" _serverUrl
-                  ])
-
-instance TF.IsProvider Provider where
-    type ProviderType Provider = "provider"
-
 newProvider
     :: P.Text -- ^ @api_key@ - 'P.apiKey'
     -> P.Text -- ^ @server_url@ - 'P.serverUrl'
@@ -85,14 +72,32 @@ newProvider _apiKey _serverUrl =
         , _serverUrl = _serverUrl
         }
 
+instance P.Hashable Provider
+
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy (Provider))
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "api_key" _apiKey
+                  , P.Just $ TF.assign "server_url" _serverUrl
+                  ])
+
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "provider"
+
+instance TF.IsValid (Provider) where
+    validator = P.mempty
+
 instance P.HasApiKey (Provider) (P.Text) where
     apiKey =
         P.lens (_apiKey :: Provider -> P.Text)
-               (\s a -> s { _apiKey = a
-                          } :: Provider)
+               (\s a -> s { _apiKey = a } :: Provider)
 
 instance P.HasServerUrl (Provider) (P.Text) where
     serverUrl =
         P.lens (_serverUrl :: Provider -> P.Text)
-               (\s a -> s { _serverUrl = a
-                          } :: Provider)
+               (\s a -> s { _serverUrl = a } :: Provider)
--- provider/terrafomo-powerdns/gen/Terrafomo/PowerDNS/Resource.hs
+++ provider/terrafomo-powerdns/gen/Terrafomo/PowerDNS/Resource.hs
@@ -1,6 +1,7 @@
 -- This module is auto-generated.
 
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE StrictData        #-}
 
@@ -24,7 +25,6 @@ module Terrafomo.PowerDNS.Resource
     ) where
 
 import Data.Functor ((<$>))
-import Data.Maybe   (catMaybes)
 
 import GHC.Base (($))
 
@@ -32,7 +32,10 @@ import Terrafomo.PowerDNS.Settings
 
 import qualified Data.Hashable               as P
 import qualified Data.HashMap.Strict         as P
+import qualified Data.HashMap.Strict         as Map
 import qualified Data.List.NonEmpty          as P
+import qualified Data.Maybe                  as P
+import qualified Data.Monoid                 as P
 import qualified Data.Text                   as P
 import qualified GHC.Generics                as P
 import qualified Lens.Micro                  as P
@@ -44,6 +47,7 @@ import qualified Terrafomo.PowerDNS.Lens     as P
 import qualified Terrafomo.PowerDNS.Provider as P
 import qualified Terrafomo.PowerDNS.Types    as P
 import qualified Terrafomo.Schema            as TF
+import qualified Terrafomo.Validator         as TF
 
 -- | @powerdns_record@ Resource.
 --
@@ -53,7 +57,7 @@ data RecordResource s = RecordResource'
     { _name    :: TF.Attr s P.Text
     -- ^ @name@ - (Required)
     --
-    , _records :: TF.Attr s [TF.Attr s (TF.Attr s P.Text)]
+    , _records :: TF.Attr s [TF.Attr s P.Text]
     -- ^ @records@ - (Required)
     --
     , _ttl     :: TF.Attr s P.Integer
@@ -67,24 +71,15 @@ data RecordResource s = RecordResource'
     --
     } deriving (P.Show, P.Eq, P.Generic)
 
-instance TF.IsObject (RecordResource s) where
-    toObject RecordResource'{..} = catMaybes
-        [ TF.assign "name" <$> TF.attribute _name
-        , TF.assign "records" <$> TF.attribute _records
-        , TF.assign "ttl" <$> TF.attribute _ttl
-        , TF.assign "type" <$> TF.attribute _type'
-        , TF.assign "zone" <$> TF.attribute _zone
-        ]
-
 recordResource
     :: TF.Attr s P.Text -- ^ @name@ - 'P.name'
-    -> TF.Attr s [TF.Attr s (TF.Attr s P.Text)] -- ^ @records@ - 'P.records'
+    -> TF.Attr s [TF.Attr s P.Text] -- ^ @records@ - 'P.records'
     -> TF.Attr s P.Integer -- ^ @ttl@ - 'P.ttl'
     -> TF.Attr s P.Text -- ^ @type@ - 'P.type''
     -> TF.Attr s P.Text -- ^ @zone@ - 'P.zone'
     -> TF.Resource P.Provider (RecordResource s)
 recordResource _name _records _ttl _type' _zone =
-    TF.newResource "powerdns_record" $
+    TF.newResource "powerdns_record" TF.validator $
         RecordResource'
             { _name = _name
             , _records = _records
@@ -93,32 +88,39 @@ recordResource _name _records _ttl _type' _zone =
             , _zone = _zone
             }
 
+instance TF.IsObject (RecordResource s) where
+    toObject RecordResource'{..} = P.catMaybes
+        [ TF.assign "name" <$> TF.attribute _name
+        , TF.assign "records" <$> TF.attribute _records
+        , TF.assign "ttl" <$> TF.attribute _ttl
+        , TF.assign "type" <$> TF.attribute _type'
+        , TF.assign "zone" <$> TF.attribute _zone
+        ]
+
+instance TF.IsValid (RecordResource s) where
+    validator = P.mempty
+
 instance P.HasName (RecordResource s) (TF.Attr s P.Text) where
     name =
         P.lens (_name :: RecordResource s -> TF.Attr s P.Text)
-               (\s a -> s { _name = a
-                          } :: RecordResource s)
+               (\s a -> s { _name = a } :: RecordResource s)
 
-instance P.HasRecords (RecordResource s) (TF.Attr s [TF.Attr s (TF.Attr s P.Text)]) where
+instance P.HasRecords (RecordResource s) (TF.Attr s [TF.Attr s P.Text]) where
     records =
-        P.lens (_records :: RecordResource s -> TF.Attr s [TF.Attr s (TF.Attr s P.Text)])
-               (\s a -> s { _records = a
-                          } :: RecordResource s)
+        P.lens (_records :: RecordResource s -> TF.Attr s [TF.Attr s P.Text])
+               (\s a -> s { _records = a } :: RecordResource s)
 
 instance P.HasTtl (RecordResource s) (TF.Attr s P.Integer) where
     ttl =
         P.lens (_ttl :: RecordResource s -> TF.Attr s P.Integer)
-               (\s a -> s { _ttl = a
-                          } :: RecordResource s)
+               (\s a -> s { _ttl = a } :: RecordResource s)
 
 instance P.HasType' (RecordResource s) (TF.Attr s P.Text) where
     type' =
         P.lens (_type' :: RecordResource s -> TF.Attr s P.Text)
-               (\s a -> s { _type' = a
-                          } :: RecordResource s)
+               (\s a -> s { _type' = a } :: RecordResource s)
 
 instance P.HasZone (RecordResource s) (TF.Attr s P.Text) where
     zone =
         P.lens (_zone :: RecordResource s -> TF.Attr s P.Text)
-               (\s a -> s { _zone = a
-                          } :: RecordResource s)
+               (\s a -> s { _zone = a } :: RecordResource s)
