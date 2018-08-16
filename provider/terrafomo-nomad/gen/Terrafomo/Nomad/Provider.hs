f2e51bad125032bad071894ff1a07e56e6481fea
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:24 +0200

    Regenerating terrafomo-nomad

--- provider/terrafomo-nomad/gen/Terrafomo/Nomad/Provider.hs
+++ provider/terrafomo-nomad/gen/Terrafomo/Nomad/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.Nomad.Provider
     -- * Nomad Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * Nomad Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator   as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @Nomad@ Terraform provider configuration.
+-- | The @nomad@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/nomad/index.html terraform documentation>
 -- for more information.
@@ -107,24 +106,25 @@ newProvider _address =
         , _vaultToken = P.Nothing
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "nomad"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  P.Just $ TF.assign "address" _address
-        ,  TF.assign "ca_file" <$> _caFile
-        ,  TF.assign "cert_file" <$> _certFile
-        ,  TF.assign "key_file" <$> _keyFile
-        ,  TF.assign "region" <$> _region
-        ,  TF.assign "secret_id" <$> _secretId
-        ,  TF.assign "vault_token" <$> _vaultToken
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "address" _address
+                  , TF.assign "ca_file" <$> _caFile
+                  , TF.assign "cert_file" <$> _certFile
+                  , TF.assign "key_file" <$> _keyFile
+                  , TF.assign "region" <$> _region
+                  , TF.assign "secret_id" <$> _secretId
+                  , TF.assign "vault_token" <$> _vaultToken
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
