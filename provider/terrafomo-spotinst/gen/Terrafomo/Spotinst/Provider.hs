943bc210cd3040119673d188b2f04536ba16664d
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:27 +0200

    Regenerating terrafomo-spotinst

--- provider/terrafomo-spotinst/gen/Terrafomo/Spotinst/Provider.hs
+++ provider/terrafomo-spotinst/gen/Terrafomo/Spotinst/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.Spotinst.Provider
     -- * Spotinst Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * Spotinst Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator      as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @Spotinst@ Terraform provider configuration.
+-- | The @spotinst@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/spotinst/index.html terraform documentation>
 -- for more information.
@@ -79,19 +78,20 @@ newProvider =
         , _token = P.Nothing
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "spotinst"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  TF.assign "account" <$> _account
-        ,  TF.assign "token" <$> _token
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , TF.assign "account" <$> _account
+                  , TF.assign "token" <$> _token
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
