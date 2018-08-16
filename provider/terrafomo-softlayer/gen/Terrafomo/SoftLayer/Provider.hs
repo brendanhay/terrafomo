513c9e2232b808bcf5af1361b92d6b6a2896abc0
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 17:11:20 +0200

    Regenerating terrafomo-softlayer

--- provider/terrafomo-softlayer/gen/Terrafomo/SoftLayer/Provider.hs
+++ provider/terrafomo-softlayer/gen/Terrafomo/SoftLayer/Provider.hs
@@ -84,7 +84,7 @@ instance TF.IsProvider Provider where
     type ProviderType Provider = "softlayer"
 
 instance TF.IsObject Provider where
-    toObject x@Provider'{..} =
+    toObject Provider'{..} =
         P.catMaybes
             [ P.Just $ TF.assign "api_key" _apiKey
             , P.Just $ TF.assign "username" _username
