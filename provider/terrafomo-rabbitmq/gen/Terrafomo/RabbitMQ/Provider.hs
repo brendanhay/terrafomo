03ec4c179bf7af81772d128785f956d17f5bd4df
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:26 +0200

    Regenerating terrafomo-rabbitmq

--- provider/terrafomo-rabbitmq/gen/Terrafomo/RabbitMQ/Provider.hs
+++ provider/terrafomo-rabbitmq/gen/Terrafomo/RabbitMQ/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.RabbitMQ.Provider
     -- * RabbitMQ Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * RabbitMQ Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator      as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @RabbitMQ@ Terraform provider configuration.
+-- | The @rabbitmq@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/rabbitmq/index.html terraform documentation>
 -- for more information.
@@ -92,22 +91,23 @@ newProvider _endpoint _password _username =
         , _username = _username
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "rabbitmq"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  TF.assign "cacert_file" <$> _cacertFile
-        ,  P.Just $ TF.assign "endpoint" _endpoint
-        ,  TF.assign "insecure" <$> _insecure
-        ,  P.Just $ TF.assign "password" _password
-        ,  P.Just $ TF.assign "username" _username
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , TF.assign "cacert_file" <$> _cacertFile
+                  , P.Just $ TF.assign "endpoint" _endpoint
+                  , TF.assign "insecure" <$> _insecure
+                  , P.Just $ TF.assign "password" _password
+                  , P.Just $ TF.assign "username" _username
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
