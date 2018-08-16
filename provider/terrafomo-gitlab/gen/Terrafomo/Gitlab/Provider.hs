f873e7020c22abf63e9e3320209f62d6a799965c
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:22 +0200

    Regenerating terrafomo-gitlab

--- provider/terrafomo-gitlab/gen/Terrafomo/Gitlab/Provider.hs
+++ provider/terrafomo-gitlab/gen/Terrafomo/Gitlab/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.Gitlab.Provider
     -- * Gitlab Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * Gitlab Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator    as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @Gitlab@ Terraform provider configuration.
+-- | The @gitlab@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/gitlab/index.html terraform documentation>
 -- for more information.
@@ -91,21 +90,22 @@ newProvider _token =
         , _token = _token
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "gitlab"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  TF.assign "base_url" <$> _baseUrl
-        ,  TF.assign "cacert_file" <$> _cacertFile
-        ,  P.Just $ TF.assign "insecure" _insecure
-        ,  P.Just $ TF.assign "token" _token
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , TF.assign "base_url" <$> _baseUrl
+                  , TF.assign "cacert_file" <$> _cacertFile
+                  , P.Just $ TF.assign "insecure" _insecure
+                  , P.Just $ TF.assign "token" _token
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
