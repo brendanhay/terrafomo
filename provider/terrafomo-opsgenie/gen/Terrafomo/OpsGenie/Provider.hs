f4de2aaff879a0fe605e3858c48365fe53984abf
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:25 +0200

    Regenerating terrafomo-opsgenie

--- provider/terrafomo-opsgenie/gen/Terrafomo/OpsGenie/Provider.hs
+++ provider/terrafomo-opsgenie/gen/Terrafomo/OpsGenie/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.OpsGenie.Provider
     -- * OpsGenie Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * OpsGenie Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator      as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @OpsGenie@ Terraform provider configuration.
+-- | The @opsgenie@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/opsgenie/index.html terraform documentation>
 -- for more information.
@@ -74,18 +73,19 @@ newProvider _apiKey =
         { _apiKey = _apiKey
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "opsgenie"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  P.Just $ TF.assign "api_key" _apiKey
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "api_key" _apiKey
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
