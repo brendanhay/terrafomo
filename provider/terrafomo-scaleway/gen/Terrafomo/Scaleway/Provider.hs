2f22f5d00a989b793a8cca6caa10383eebb8c1fe
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:26 +0200

    Regenerating terrafomo-scaleway

--- provider/terrafomo-scaleway/gen/Terrafomo/Scaleway/Provider.hs
+++ provider/terrafomo-scaleway/gen/Terrafomo/Scaleway/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.Scaleway.Provider
     -- * Scaleway Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * Scaleway Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator      as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @Scaleway@ Terraform provider configuration.
+-- | The @scaleway@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/scaleway/index.html terraform documentation>
 -- for more information.
@@ -86,20 +85,21 @@ newProvider _organization _token =
         , _token = _token
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "scaleway"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  P.Just $ TF.assign "organization" _organization
-        ,  TF.assign "region" <$> _region
-        ,  P.Just $ TF.assign "token" _token
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "organization" _organization
+                  , TF.assign "region" <$> _region
+                  , P.Just $ TF.assign "token" _token
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
