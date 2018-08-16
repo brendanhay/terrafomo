a310256a5dc21bb63344e8d749408bbc41631b2b
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:22 +0200

    Regenerating terrafomo-fastly

--- provider/terrafomo-fastly/gen/Terrafomo/Fastly/Provider.hs
+++ provider/terrafomo-fastly/gen/Terrafomo/Fastly/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.Fastly.Provider
     -- * Fastly Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * Fastly Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator    as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @Fastly@ Terraform provider configuration.
+-- | The @fastly@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/fastly/index.html terraform documentation>
 -- for more information.
@@ -79,19 +78,20 @@ newProvider =
         , _baseUrl = P.Nothing
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "fastly"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  TF.assign "api_key" <$> _apiKey
-        ,  TF.assign "base_url" <$> _baseUrl
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , TF.assign "api_key" <$> _apiKey
+                  , TF.assign "base_url" <$> _baseUrl
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
