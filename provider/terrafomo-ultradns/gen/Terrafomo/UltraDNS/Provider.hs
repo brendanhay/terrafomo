504c22294b5cadfaa944d2854546318c0e7a8c55
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:27 +0200

    Regenerating terrafomo-ultradns

--- provider/terrafomo-ultradns/gen/Terrafomo/UltraDNS/Provider.hs
+++ provider/terrafomo-ultradns/gen/Terrafomo/UltraDNS/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.UltraDNS.Provider
     -- * UltraDNS Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * UltraDNS Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator      as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @UltraDNS@ Terraform provider configuration.
+-- | The @ultradns@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/ultradns/index.html terraform documentation>
 -- for more information.
@@ -86,20 +85,21 @@ newProvider _password _username =
         , _username = _username
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "ultradns"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  P.Just $ TF.assign "baseurl" _baseurl
-        ,  P.Just $ TF.assign "password" _password
-        ,  P.Just $ TF.assign "username" _username
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "baseurl" _baseurl
+                  , P.Just $ TF.assign "password" _password
+                  , P.Just $ TF.assign "username" _username
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
