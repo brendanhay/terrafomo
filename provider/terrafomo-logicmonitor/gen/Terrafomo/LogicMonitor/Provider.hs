7ecb3bd38d702c6e47bce1ce8e81d65519c078b5
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:24 +0200

    Regenerating terrafomo-logicmonitor

--- provider/terrafomo-logicmonitor/gen/Terrafomo/LogicMonitor/Provider.hs
+++ provider/terrafomo-logicmonitor/gen/Terrafomo/LogicMonitor/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.LogicMonitor.Provider
     -- * LogicMonitor Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * LogicMonitor Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator          as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @LogicMonitor@ Terraform provider configuration.
+-- | The @logicmonitor@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/logicmonitor/index.html terraform documentation>
 -- for more information.
@@ -84,20 +83,21 @@ newProvider _apiId _apiKey _company =
         , _company = _company
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "logicmonitor"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  P.Just $ TF.assign "api_id" _apiId
-        ,  P.Just $ TF.assign "api_key" _apiKey
-        ,  P.Just $ TF.assign "company" _company
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "api_id" _apiId
+                  , P.Just $ TF.assign "api_key" _apiKey
+                  , P.Just $ TF.assign "company" _company
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
