20b0f777aa9f585db314e104e1f1fecd65287d0f
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:23 +0200

    Regenerating terrafomo-ignition

--- provider/terrafomo-ignition/gen/Terrafomo/Ignition/Provider.hs
+++ provider/terrafomo-ignition/gen/Terrafomo/Ignition/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.Ignition.Provider
     -- * Ignition Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * Ignition Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator      as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @Ignition@ Terraform provider configuration.
+-- | The @ignition@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/ignition/index.html terraform documentation>
 -- for more information.
@@ -68,16 +67,18 @@ newProvider
 newProvider =
     Provider'
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "ignition"
 
-instance TF.IsObject Provider where
-    toObject Provider' = []
+instance TF.IsSection Provider where
+    toSection x@Provider' =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
