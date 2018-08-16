3a4d24e32918d012f7d30cfcd42e3308953c65be
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 17:11:17 +0200

    Regenerating terrafomo-newrelic

--- provider/terrafomo-newrelic/gen/Terrafomo/NewRelic/Provider.hs
+++ provider/terrafomo-newrelic/gen/Terrafomo/NewRelic/Provider.hs
@@ -85,7 +85,7 @@ instance TF.IsProvider Provider where
     type ProviderType Provider = "newrelic"
 
 instance TF.IsObject Provider where
-    toObject x@Provider'{..} =
+    toObject Provider'{..} =
         P.catMaybes
             [ P.Just $ TF.assign "api_key" _apiKey
             , TF.assign "api_url" <$> _apiUrl
