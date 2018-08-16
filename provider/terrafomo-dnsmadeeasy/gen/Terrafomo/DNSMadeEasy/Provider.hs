376a1bb84a7ab7252f929643503d4d0ef90555f7
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:21 +0200

    Regenerating terrafomo-dnsmadeeasy

--- provider/terrafomo-dnsmadeeasy/gen/Terrafomo/DNSMadeEasy/Provider.hs
+++ provider/terrafomo-dnsmadeeasy/gen/Terrafomo/DNSMadeEasy/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.DNSMadeEasy.Provider
     -- * DNSMadeEasy Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * DNSMadeEasy Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator         as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @DNSMadeEasy@ Terraform provider configuration.
+-- | The @dme@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/dme/index.html terraform documentation>
 -- for more information.
@@ -87,20 +86,21 @@ newProvider _akey _skey _usesandbox =
         , _usesandbox = _usesandbox
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "dme"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  P.Just $ TF.assign "akey" _akey
-        ,  P.Just $ TF.assign "skey" _skey
-        ,  P.Just $ TF.assign "usesandbox" _usesandbox
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "akey" _akey
+                  , P.Just $ TF.assign "skey" _skey
+                  , P.Just $ TF.assign "usesandbox" _usesandbox
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
