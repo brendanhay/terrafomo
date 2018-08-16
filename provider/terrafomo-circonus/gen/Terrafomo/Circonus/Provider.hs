a43fb838bc81fe76a364395ee92ac19796a4b8de
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:20 +0200

    Regenerating terrafomo-circonus

--- provider/terrafomo-circonus/gen/Terrafomo/Circonus/Provider.hs
+++ provider/terrafomo-circonus/gen/Terrafomo/Circonus/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.Circonus.Provider
     -- * Circonus Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * Circonus Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator      as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @Circonus@ Terraform provider configuration.
+-- | The @circonus@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/circonus/index.html terraform documentation>
 -- for more information.
@@ -86,20 +85,21 @@ newProvider _key =
         , _key = _key
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "circonus"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  P.Just $ TF.assign "api_url" _apiUrl
-        ,  P.Just $ TF.assign "auto_tag" _autoTag
-        ,  P.Just $ TF.assign "key" _key
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "api_url" _apiUrl
+                  , P.Just $ TF.assign "auto_tag" _autoTag
+                  , P.Just $ TF.assign "key" _key
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
