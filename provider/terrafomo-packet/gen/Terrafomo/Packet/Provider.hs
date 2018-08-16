2304d80af246333f75505bce760aa26cd4234eb1
Brendan Hay <brendan.g.hay@gmail.com>
2018-08-16 13:44:25 +0200

    Regenerating terrafomo-packet

--- provider/terrafomo-packet/gen/Terrafomo/Packet/Provider.hs
+++ provider/terrafomo-packet/gen/Terrafomo/Packet/Provider.hs
@@ -20,7 +20,6 @@ module Terrafomo.Packet.Provider
     -- * Packet Provider Datatype
       Provider (..)
     , newProvider
-    , defaultProvider
 
     -- * Packet Specific Aliases
     , DataSource
@@ -56,7 +55,7 @@ import qualified Terrafomo.Validator    as TF
 type DataSource a = TF.Schema ()               Provider a
 type Resource   a = TF.Schema (TF.Lifecycle a) Provider a
 
--- | The @Packet@ Terraform provider configuration.
+-- | The @packet@ Terraform provider configuration.
 --
 -- See the <https://www.terraform.io/docs/providers/packet/index.html terraform documentation>
 -- for more information.
@@ -75,18 +74,19 @@ newProvider _authToken =
         { _authToken = _authToken
         }
 
-defaultProvider :: TF.Provider (P.Maybe Provider)
-defaultProvider =
-    TF.Provider
-        { _providerType   = TF.Type P.Nothing "provider"
-        , _providerAlias  = P.Nothing
-        , _providerConfig = P.Nothing
-        }
+instance TF.IsProvider Provider where
+    type ProviderType Provider = "packet"
 
-instance TF.IsObject Provider where
-    toObject Provider'{..} = P.catMaybes
-        [  P.Just $ TF.assign "auth_token" _authToken
-        ]
+instance TF.IsSection Provider where
+    toSection x@Provider'{..} =
+        let typ = TF.providerType (Proxy :: Proxy Provider)
+            key = TF.providerKey x
+         in TF.section "provider" [TF.type_ typ]
+          & TF.pairs
+              (P.catMaybes
+                  [ P.Just $ TF.assign "alias" (TF.toValue (TF.keyName key))
+                  , P.Just $ TF.assign "auth_token" _authToken
+                  ])
 
 instance TF.IsValid (Provider) where
     validator = P.mempty
