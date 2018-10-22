8225792a256b44c649c8fa298d68668dd64a55bb
Brian McKenna <brian@brianmckenna.org>
2018-10-22 22:46:14 +1100

    Fix missing commas at end of pairs in JSON objects

--- terrafomo/test/fixtures/locals_iam_policy.tf
+++ terrafomo/test/fixtures/locals_iam_policy.tf
@@ -3,25 +3,25 @@ locals {
 {
   "Statement": [
     {
-      "Effect": "Allow"
+      "Effect": "Allow",
       "Action": [
         "iam:ListUsers",
         "iam:GetAccountPasswordPolicy"
-      ]
+      ],
       "Resource": "*"
     },
     {
-      "Effect": "Allow"
+      "Effect": "Allow",
       "Action": [
         "iam:*AccessKey*",
         "iam:ChangePassword",
         "iam:GetUser",
         "iam:*ServiceSpecificCredential*",
         "iam:*SigningCertificate*"
-      ]
+      ],
       "Resource": "arn:aws:iam::*:user/foobarbaz"
     }
-  ]
+  ],
   "Version": "2012-10-17"
 }
 EOF
